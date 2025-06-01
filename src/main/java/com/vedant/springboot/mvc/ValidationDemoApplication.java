0b586c2b2c3fa2277b84a79162788e5981747e3c
vedant130699 <vedantkulkarni1306@gmail.com>
2025-06-01 12:18:28 +0530

    first commit

---
+++ src/main/java/com/vedant/springboot/mvc/ValidationDemoApplication.java
@@ -0,0 +1,13 @@
+package com.vedant.springboot.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidationDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ValidationDemoApplication.class, args);
+    }
+
+}
